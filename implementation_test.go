f8a8e16055629f2ca15319de66ea68bdd33af413
Andriyaschenko Vladimir <vovan4ik041@gmail.com>
2023-05-29 00:05:35 +0300

    Update implementation_test.go

--- implementation_test.go
+++ implementation_test.go
@@ -3,19 +3,46 @@ package lab2
 import (
 	"fmt"
 	"testing"
+
+	. "gopkg.in/check.v1"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
-	}
+func Test(t testing.T) {
+	TestingT(t)
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+type mySuite struct{}
+
+var _ = Suite(&mySuite{})
+
+func (smySuite) TestEmpty(c C) {
+	res, err := PostfixToPrefix("")
+	c.Assert(res, Equals, "")
+	c.Assert(err, ErrorMatches, "invalid expression")
+}
+
+func (smySuite) TestInvalid(c C) {
+	res, err := PostfixToPrefix("2 +")
+	c.Assert(res, Equals, "")
+	c.Assert(err, ErrorMatches, "not enough operands")
+}
+
+func (smySuite) TestSimplePrefixExpression(c C) {
+	res, err := PostfixToPrefix("9 3 /")
+	c.Assert(res, Equals, "/ 9 3")
+	c.Assert(err, IsNil)
+}
+
+func (smySuite) TestComplexPrefixExpression(c C) {
+	res, err := PostfixToPrefix("1 9 + 7 3 - 5 2 ^ + 8 4 / - 6 + ")
+	c.Assert(res, Equals, " + 1 - 9 + 7 3 - ^ 5 2 + / 8 4 6")
+	c.Assert(err, IsNil)
+}
+
+func ExamplePostfixToPrefix() {
+	res, _ := PostfixToPrefix("2 2 +")
 	fmt.Println(res)
 
 	// Output:
-	// 2 2 +
+	// + 2 2
 }
